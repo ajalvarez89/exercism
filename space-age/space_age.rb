e2827894328c1c9ab9de49897a2dd0b6a81ef530
Alvaro Alvarez <alvaro.alvarez@youse.com.co>
2019-09-25 17:03:59 -0500

    optimizing the code

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -1,17 +1,19 @@
 require 'byebug'
 
 class SpaceAge
+
   attr_reader :seconds
-  
+
+  PERIOD_HEARD = 31557600.00
   PERIOD = {
-        :hearth => 365.25,
-        :mercury => 0.2408467,
-        :venus => 0.61519726,
-        :mars => 1.8808158,
-        :jupiter => 11.862615,
-        :saturn => 29.447498,
-        :uranus => 84.016846,
-        :neptune => 164.79132,
+        :hearth => PERIOD_HEARD,
+        :mercury => PERIOD_HEARD * 0.2408467,
+        :venus => PERIOD_HEARD * 0.61519726,
+        :mars => PERIOD_HEARD * 1.8808158,
+        :jupiter => PERIOD_HEARD * 11.862615,
+        :saturn => PERIOD_HEARD * 29.447498,
+        :uranus => PERIOD_HEARD * 84.016846,
+        :neptune => PERIOD_HEARD * 164.79132
       }
 
   def initialize (seconds)
@@ -19,44 +21,34 @@ class SpaceAge
   end
 
   def on_earth
-    @seconds/calculate_sec(PERIOD[:hearth])
+    @seconds/PERIOD[:hearth]
   end
 
   def on_mercury
-    @seconds/calculate_sec(calculate_day(PERIOD[:mercury]))
+    @seconds/PERIOD[:mercury]
   end
   
   def on_venus
-    @seconds/calculate_sec(calculate_day(PERIOD[:venus]))
+    @seconds/PERIOD[:venus]
   end
 
   def on_mars
-    @seconds/calculate_sec(calculate_day(PERIOD[:mars]))
+    @seconds/PERIOD[:mars]
   end
   
   def on_jupiter
-    @seconds/calculate_sec(calculate_day(PERIOD[:jupiter]))
+    @seconds/PERIOD[:jupiter]
   end
 
   def on_saturn
-    @seconds/calculate_sec(calculate_day(PERIOD[:saturn]))
+    @seconds/PERIOD[:saturn]
   end
 
   def on_uranus
-    @seconds/calculate_sec(calculate_day(PERIOD[:uranus]))
+    @seconds/PERIOD[:uranus]
   end
 
   def on_neptune
-    @seconds/calculate_sec(calculate_day(PERIOD[:neptune]))
-  end
-
-  private
-
-  def calculate_day(period)
-    days = (period * 365.25)/1
-  end
-
-  def calculate_sec(days)
-    days * 86400.00
+    @seconds/PERIOD[:neptune]
   end
 end
