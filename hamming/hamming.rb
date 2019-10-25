ea1caebd1cb619750e770c11fbeeed425973e6c0
Alvaro Alvarez <alvaro.alvarez@youse.com.co>
2019-10-25 15:25:16 -0500

    writing a second solution

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -9,30 +9,30 @@ require 'byebug'
 
 class Hamming
 
-  def self.compute(secuence_one,secuence_two)
-    raise ArgumentError if secuence_one.length != secuence_two.length
-
-    hamming_result = 0
-
-    secuence_one.each_char.with_index do |nucleotide, index|
-      hamming_result += 1 if nucleotide != secuence_two[index]
-    end
-    hamming_result
-  end
-
   # def self.compute(secuence_one,secuence_two)
   #   raise ArgumentError if secuence_one.length != secuence_two.length
-  #   index = 0
-  #   result = 0
 
-  #   while index < secuence_one.length
-  #     if secuence_one[index] != secuence_two[index]
-  #       result += 1
-  #     end
-  #     index += 1
+  #   hamming_result = 0
+
+  #   secuence_one.each_char.with_index do |nucleotide, index|
+  #     hamming_result += 1 if nucleotide != secuence_two[index]
   #   end
-  #   return result
+  #   hamming_result
   # end
+
+  def self.compute(secuence_one,secuence_two)
+    raise ArgumentError if secuence_one.length != secuence_two.length
+    index = 0
+    result = 0
+
+    while index < secuence_one.length
+      if secuence_one[index] != secuence_two[index]
+        result += 1
+      end
+      index += 1
+    end
+    return result
+  end
 end
 
 
